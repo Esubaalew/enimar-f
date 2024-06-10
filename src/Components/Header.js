08ad0f6299368d22497ae2220f31815f81aef0fe
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-06-10 22:58:27 +0300

    improvements

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,35 @@
+// Header.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faBook, faComments } from '@fortawesome/free-solid-svg-icons';
+import '../styles/Header.css';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Acme Learn</h1>
+    <header className="Hheader">
+      <div className="Hlogo">
+        <div className="Hlogo-bing">A</div> {/* Bing Icon Placeholder */}
+        <span className="Hlogo-text">Acme</span>
+      </div>
+      <nav className="Hnav">
+        <ul className="Hnav-list">
+          <li className="Hnav-item">
+            <a href="#users" className="Hnav-link">
+              <FontAwesomeIcon icon={faUser} className="Hnav-icon" /> Users
+            </a>
+          </li>
+          <li className="Hnav-item">
+            <a href="#courses" className="Hnav-link">
+              <FontAwesomeIcon icon={faBook} className="Hnav-icon" /> Courses
+            </a>
+          </li>
+          <li className="Hnav-item">
+            <a href="#chat" className="Hnav-link">
+              <FontAwesomeIcon icon={faComments} className="Hnav-icon" /> Chat
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
