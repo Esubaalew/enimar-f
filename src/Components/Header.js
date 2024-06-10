a4195e1ae21f0e1b7d71b70927f16729dd0f85de
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-06-10 23:03:58 +0300

    fuckin header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,30 @@
-// Header.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faBook, faComments } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faBook, faComments, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { getLoggedInUser } from '../API/auth';
 import '../styles/Header.css';
 
 const Header = () => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const accessToken = JSON.parse(localStorage.getItem('user'))?.access;
+    if (accessToken) {
+      getLoggedInUser(accessToken)
+        .then(userData => {
+          setUsername(userData.username);
+        })
+        .catch(error => {
+          console.error('Error fetching user data:', error);
+        });
+    }
+  }, []);
+
   return (
     <header className="Hheader">
       <div className="Hlogo">
-        <div className="Hlogo-bing">A</div> {/* Bing Icon Placeholder */}
-        <span className="Hlogo-text">Acme</span>
+        <div className="Hlogo-bing">E</div> {/* Bing Icon Placeholder */}
+        <span className="Hlogo-text">Enimar</span>
       </div>
       <nav className="Hnav">
         <ul className="Hnav-list">
@@ -28,6 +43,13 @@ const Header = () => {
               <FontAwesomeIcon icon={faComments} className="Hnav-icon" /> Chat
             </a>
           </li>
+          {username && (
+            <li className="Hnav-item">
+              <a href={`/user/${username}`} className="Hnav-link">
+                <FontAwesomeIcon icon={faUserCircle} className="Hnav-icon" /> Me
+              </a>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
