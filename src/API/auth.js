6613e32aee62078de1fb0271efd5c2744d700751
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-06-13 22:20:48 +0300

    password reset apis

--- src/API/auth.js
+++ src/API/auth.js
@@ -50,3 +50,24 @@ export const signUpStudent = async (teacherData) => {
       throw error.response.data;
     }
   };
+
+
+  // Function to request password reset
+export const requestPasswordReset = async (emailData) => {
+  try {
+    const response = await axios.post(`${domain}api/password-reset/`, emailData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+// Function to confirm password reset
+export const confirmPasswordReset = async (uidb64, token, newPasswordData) => {
+  try {
+    const response = await axios.post(`${domain}api/password-reset-confirm/${uidb64}/${token}/`, newPasswordData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
