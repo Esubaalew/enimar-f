106693cd8a5ba5e5d04469e45dc4dd1c77716cb7
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-06-14 10:34:19 +0300

    user list icons

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ProfileIcon from './ProfileIcon';
 import '../styles/UserList.css';
 
 const UserList = ({ users }) => {
-
   const filteredUsers = users.filter(user => user.is_teacher || user.is_student);
+
+  const getInitials = (firstName, lastName) => {
+    return `${firstName[0]}${lastName[0]}`;
+  };
 
   return (
     <div className="user-list">
-      <h1> Suggested Users</h1>
+      <h1>Suggested Users</h1>
       <div className="user-list-container">
         {filteredUsers.map(user => (
           <div key={user.id} className="user-card">
-            <div className="user-avatar">
-              <ProfileIcon firstName={user?.first_name} lastName={user?.last_name} />
+            <div className="initials-avatar">
+              {getInitials(user.first_name, user.last_name)}
             </div>
             <div className="user-info">
               <h3>{user.first_name + " " + user.last_name}</h3>
