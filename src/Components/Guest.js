9d74066f44ad824cd1c60586162da183b23fd231
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-06-15 00:46:25 +0300

    Guest moving

--- src/Components/Guest.js
+++ src/Components/Guest.js
@@ -1,25 +1,38 @@
+import React, { useEffect } from 'react';
 import '../styles/Guest.css';
 import guestImage from '../assets/images/guestL.jpeg';
 import { useNavigate } from 'react-router-dom';
 
 const Guest = () => {
-    const navigate = useNavigate();
-    const handleSSignUp = () => {
-      navigate('/StudentSignUp');
-    };
-    const handleTSignUp = () => {
-        navigate('/TeacherSignUp');
-        }
-    const handleSignIn = () => {
-        navigate('/in');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loggedInUser = JSON.parse(localStorage.getItem('user'));
+    if (loggedInUser) {
+      navigate('/home');
     }
+  }, [navigate]);
+
+  const handleSSignUp = () => {
+    navigate('/StudentSignUp');
+  };
+
+  const handleTSignUp = () => {
+    navigate('/TeacherSignUp');
+  };
+
+  const handleSignIn = () => {
+    navigate('/in');
+  };
 
   return (
     <div className="guest-container">
       <header className="guest-header">
         <div className="logo">Enimar Code Learning</div>
         <nav className="nav-links">
-          <button onClick={handleSignIn} className="btn-signin">Sign In</button>
+          <button onClick={handleSignIn} className="btn-signin">
+            Sign In
+          </button>
           <button className="btn-signup">Sign Up</button>
         </nav>
       </header>
@@ -29,12 +42,16 @@ const Guest = () => {
           <div className="intro-text">
             <h1>Unlock Your Coding Potential</h1>
             <p>
-              Join our vibrant community of developers and share your ideas, learn
-              from experts, and bring your projects to life.
+              Join our vibrant community of developers and share your ideas,
+              learn from experts, and bring your projects to life.
             </p>
             <div className="buttons">
-              <button className="btn-primary" onClick={handleSSignUp}>Learn Today</button>
-              <button className="btn-secondary" onClick={handleTSignUp} >Start Teaching</button>
+              <button className="btn-primary" onClick={handleSSignUp}>
+                Learn Today
+              </button>
+              <button className="btn-secondary" onClick={handleTSignUp}>
+                Start Teaching
+              </button>
             </div>
           </div>
           <div className="intro-image">
@@ -47,15 +64,23 @@ const Guest = () => {
         <div className="features">
           <div className="feature">
             <h3>Learn to Code</h3>
-            <p>Explore a wide range of coding tutorials and resources to expand your skills.</p>
+            <p>
+              Explore a wide range of coding tutorials and resources to expand
+              your skills.
+            </p>
           </div>
           <div className="feature">
             <h3>Share Your Ideas</h3>
-            <p>Collaborate with like-minded individuals and bring your innovative ideas to life.</p>
+            <p>
+              Collaborate with like-minded individuals and bring your innovative
+              ideas to life.
+            </p>
           </div>
           <div className="feature">
             <h3>Accelerate Your Growth</h3>
-            <p>Unlock new opportunities and propel your coding career forward.</p>
+            <p>
+              Unlock new opportunities and propel your coding career forward.
+            </p>
           </div>
         </div>
       </section>
