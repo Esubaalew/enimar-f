8b1be25b7a57fe90edb7619e7062ce5f8e21ab4a
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-06-16 16:12:50 +0300

    added checkmark on the teacher - oher place

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import '../styles/Profil.css';
 
 const Profile = ({ user }) => {
-  const { first_name, last_name, bio, username } = user;
+  const { first_name, last_name, bio, username, is_teacher } = user;
   const initials = `${first_name[0]}${last_name[0]}`;
 
   return (
@@ -11,7 +11,10 @@ const Profile = ({ user }) => {
       <div className="CMprofile-card">
         <div className="CMprofile-picture">{initials}</div>
         <div className="CMprofile-info">
-          <h2>{first_name} {last_name}</h2>
+          <h2 className="CMname">
+            {first_name} {last_name}
+            {is_teacher && <span className="CMteacher-check">t &#10003;</span>}
+          </h2>
           <p className="CMusername">@{username}</p>
           <p className="CMbio">{bio}</p>
           <button className="CMfollow-button">Follow</button>
