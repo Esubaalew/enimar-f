8bdfc83da44720aa8af162525b1e2be1a693e2e3
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-20 15:47:31 +0300

    Update quiz.js

--- src/API/quiz.js
+++ src/API/quiz.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const domain = 'http://localhost:8000/';
+const domain = 'https://enimar.4gmobiles.com/';
 
 // Function to add  a question
 export const addQuestion = async (questionData, accessToken) => {
