1c080f81ab8e3e73b86fdf7dad87c3091f3e4bde
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-20 15:48:59 +0300

    Update users.js

--- src/API/users.js
+++ src/API/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const domain = ' http://127.0.0.1:8000';
+const domain = 'https://enimar.4gmobiles.com/';
 const getUserFollowers = async (userId, accessToken) => {
     try {
         const response = await axios.get(`${domain}/account/users/${userId}/followers/`, {
